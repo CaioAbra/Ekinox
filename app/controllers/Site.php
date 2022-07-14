18183fd838bee1015ce5120936177ff3ec225232
Caio Matheus dos Santos Abra <caioabra41@gmail.com>
2022-07-13 23:32:15 -0300

    construção e teste de rotas

---
+++ app/controllers/Site.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace app\controllers;
+
+class Site
+{
+    public function home(){
+        echo "bem vindo a home";
+    }
+}
--- core/Router.php
+++ core/Router.php
@@ -4,8 +4,34 @@ namespace core;
 
 class Router
 {
+    private $controller = 'Site';
+    private $method = 'home';
+    private $param = [];
+
     public function __construct()
     {
-        echo 'Carrengando a classe Router';
+        $router = $this->url();
+        if (file_exists('app/cotrollers/' . ucfirst($router[0]) . 'php')) :
+            $this->controller = $router[0];
+            unset($router[0]);
+        endif;
+
+        $class = "\\app\\controllers\\" . ucfirst($this->controller);
+        $object = new $class;
+
+        if (isset($router[1]) and method_exists($class, $router[1])) :
+            $this->method = $router[1];
+            unset($router[1]);
+        endif;
+
+        $this->param = $router ? array_values($router) : [];
+
+        call_user_func_array([$object, $this->method], $this->param);
+    }
+
+    private function url()
+    {
+        $parse_url = explode("/", filter_input(INPUT_GET, 'router', FILTER_SANITIZE_URL));
+        return $parse_url;
     }
 }
